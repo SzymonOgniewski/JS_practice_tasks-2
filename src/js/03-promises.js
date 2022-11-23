fb6ffe65e4b57e7d5e83e7f0bb050424df97e5fb
Szymon Ogniewski <szymonogniewski00@gmail.com>
2022-11-23 14:11:35 +0100

    task 3 in prog

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  console.log(shouldResolve);
+
+  const promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      );
+      // Fulfill
+    } else {
+      reject(
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+      // Reject
+    }
+  });
 }
+console.log(createPromise(1, 1500));
+
+// form.addEventListener('sumbit', () => {
+//   preventDefault();
+//   const { elements:{startDelay, stepDelay, amount} = event.currentTarget}
+//   setTimeout(() => {
+//     let i = 1
+
+//     while (i <= amount) {
+//       position === i;
+//       i++
+//       createPromise(delay===stepDelay)
+//     }
+//   },startDelay);
+// });
